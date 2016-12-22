78752529d760de55e7730d27abb256be9900867d
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-12-22 16:12:27 +0900

    Add task controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources 'poems', :except => [:new, :edit]
   resources 'reports', :except => [:new, :edit]
+  resources 'tasks', :except => [:new, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
