e565c2a6882538d8b870ae8125e8f97934db1463
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-12-22 11:24:58 +0900

    Add seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create({
+  name: 'test user',
+  email: 'test@te.st',
+  email_confirmation: 'test@te.st',
+  password: 'password',
+})
+
+Poem.create([
+  {
+    user: user,
+    date: Date.today,
+    status: 0,
+    body: 'test poem',
+  },
+  {
+    user: user,
+    date: Date.today,
+    status: 1,
+    body: 'test poem2',
+  },
+])
+
+Report.create([
+  {
+    user: user,
+    date: Date.today,
+    status: 0,
+    body: 'test report body',
+    title: 'test report title',
+  },
+  {
+    user: user,
+    date: Date.today,
+    status: 1,
+    body: 'test report2 body',
+    title: 'test report2 title',
+  },
+])
+
+Task.create([
+  {
+    user: user,
+    date: Date.today,
+    status: 0,
+    body: 'test task',
+    priority: 0,
+  },
+  {
+    user: user,
+    date: Date.today,
+    status: 1,
+    body: 'test task2',
+    priority: 1,
+  },
+  {
+    user: user,
+    date: Date.today,
+    status: 2,
+    body: 'test task3',
+    priority: 0,
+  },
+])
