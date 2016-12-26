b02b13ed20cbbe0208df7b6aaa544eff3d32de10
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-12-26 11:29:28 +0900

    Add rack-cors code

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,13 @@ module Nippo2
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete]
+      end
+    end
+
     # timezone
     config.time_zone = 'Tokyo'
   end
