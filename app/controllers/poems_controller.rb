95ad34dd712f7bd22849421adb80f38428ba7a9a
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-12-31 00:43:37 +0900

    Comentout auth

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,5 @@
 class PoemsController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
   before_action :set_poem, only: [:show, :update, :destroy]
 
   def index
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
   before_action :set_task, only: [:show, :update, :destroy]
 
   def index
